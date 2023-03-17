b2441ecbe131f82b38fb64412b395d1c225cc6ee
Safuan <elmail.safuan@gmail.com>
2023-03-16 17:33:09 -0700

    printf is here

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the function of main
+ *
+ * Return: it should always be 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
