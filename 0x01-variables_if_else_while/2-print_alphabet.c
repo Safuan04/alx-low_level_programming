77da7cce42de9c6a3e502762b1a7fdcc7d788e35
Safuan <elmail.safuan@gmail.com>
2023-03-17 15:12:30 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this is a function
+ *
+ * Return: it sgould be 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		putchar(a);
+		putchar('\n');
+	}
+
+	return (0);
+}
