04919e0300c804d073ec563543e5761d2eace813
Safuan <elmail.safuan@gmail.com>
2023-03-21 17:12:09 -0700

    all alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_alphabet - this is a function
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
