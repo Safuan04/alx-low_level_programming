0234465ec41ae9ee596074f4aec789b995c235f6
Safuan <elmail.safuan@gmail.com>
2023-03-22 17:30:33 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - this is a function
+ * @n: this is a parameter
+ * Return: should be n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -(n);
+	}
+	else
+	{
+		n = n;
+	}
+	return (n);
+}
