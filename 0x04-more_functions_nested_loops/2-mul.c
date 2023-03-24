015a198084da2de90b0039ef13210f0889fa0c16
Safuan <elmail.safuan@gmail.com>
2023-03-24 10:54:30 -0700

    multiplication bb

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,12 +3,12 @@
  * mul - this is a function for multiplaction
  * @a: this is a parameter
  * @b: this is a parameter
- * return: this should be n
+ * Return: this should be n
  */
 int mul(int a, int b)
 {
 	int n;
-	
+
 	n = a * b;
 	return (n);
 }
