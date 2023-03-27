737e8bf85f00880f8576ca354fd552f02677f4b3
Safuan <elmail.safuan@gmail.com>
2023-03-27 08:40:12 -0700

    reverse print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - this is a function that prints the arrays in reverse
+ * @s: this is a parameter
+ */
+void print_rev(char *s)
+{
+	int i;
+	int count;
+
+	count = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		count++;
+	}
+	for (i = count ; i >= 0 ; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
