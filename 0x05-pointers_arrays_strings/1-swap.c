975b4343ec9b6d8baab92244d08808d2f4efa12a
Safuan <elmail.safuan@gmail.com>
2023-03-27 07:33:16 -0700

    swap numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * swap_int - this is a function that swaps numbers
+ * @a: this is a parameter
+ * @b: this is a parameter
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
