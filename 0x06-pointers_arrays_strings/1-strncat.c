e2b35326a6c9adc112ee157b69026b80d6052cd2
Safuan <elmail.safuan@gmail.com>
2023-03-30 16:42:05 -0700

    concat 2 string with n limit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - this is a function that concatinate two strings
+ * @dest: this is a parameter
+ * @src: this is a parameter
+ * @n: this is a parameter
+ * Return: this should return dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int destl = 0;
+	int srcl = 0;
+	int i;
+
+	for (i = 0; dest[i] != '\0' ; i++)
+	{
+		destl++;
+	}
+	for (i = 0; src[i] != '\0' ; i++)
+	{
+		srcl++;
+	}
+	for (i = 0; i < n ; i++)
+	{
+		dest[destl + i] = src[i];
+	}
+	return (dest);
+}
