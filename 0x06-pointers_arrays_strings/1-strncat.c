81049a0f8d859ee7739f36c5a7784221d322bf9b
Safuan <elmail.safuan@gmail.com>
2023-03-30 16:44:24 -0700

    concat 2 string with n limit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,15 +12,15 @@ char *_strncat(char *dest, char *src, int n)
 	int srcl = 0;
 	int i;
 
-	for (i = 0; dest[i] != '\0' ; i++)
+	for (i = 0 ; dest[i] != '\0' ; i++)
 	{
 		destl++;
 	}
-	for (i = 0; src[i] != '\0' ; i++)
+	for (i = 0 ; src[i] != '\0' ; i++)
 	{
 		srcl++;
 	}
-	for (i = 0; i < n ; i++)
+	for (i = 0 ; i < n ; i++)
 	{
 		dest[destl + i] = src[i];
 	}
