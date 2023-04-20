8030fc49ac09715d4b4c3deb473c14f9a4ed1d4e
Safuan <elmail.safuan@gmail.com>
2023-04-20 09:28:32 -0700

    print numbers where seperator is between them

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - this is a function that prints numbers
+ * @separator: this is a separator between numbers
+ * @n: this is the number of arg to be printed
+ * Return: null
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list pr;
+	unsigned int i, x;
+
+	va_start(pr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+		{
+			x = va_arg(pr, int);
+			printf("%d", x);
+		}
+		else if ( separator != NULL)
+		{
+			x = va_arg(pr, int);
+			printf("%d%s", x, separator);
+		}
+	}
+		printf("\n");
+
+		va_end(pr);
+}
