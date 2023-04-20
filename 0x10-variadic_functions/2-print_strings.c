2fd08c564f4c8756e1f2ad71b014a6bae84fb6ee
Safuan <elmail.safuan@gmail.com>
2023-04-20 10:53:14 -0700

    printarguments of strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		x = va_arg(str, char*);
 		if (x == NULL)
 		{
-			printf("nil");
+			printf("(nil)");
 		}
 		else
 			printf("%s", x);
