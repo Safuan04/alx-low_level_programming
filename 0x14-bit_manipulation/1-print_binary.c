9b76e6ed7272f784111f0768b62f50e7cf384928
Safuan04 <elmail.safuan@gmail.com>
2023-05-04 12:23:42 -0700

    convert dec to bi

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_binary - this is a function that converts dec num to binary
+ * @n: the number to be converted
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
