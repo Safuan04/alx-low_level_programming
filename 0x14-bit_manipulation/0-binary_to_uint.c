666ab5be2fc64a6cc45c358f44afe6e62a041510
Safuan04 <elmail.safuan@gmail.com>
2023-05-04 09:58:24 -0700

    bi to dec

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <math.h>
+/**
+ * binary_to_uint - this is a function that converts a binary num to a dec
+ * @b: this is a parameter
+ * Return: is rslt
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	int dec = 0;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if ( b[i] < '0' || b[i] > '1')
+			return (0);
+		else
+			dec = dec * 2 + (b[i] - '0');
+	}
+	return (dec);
+}
