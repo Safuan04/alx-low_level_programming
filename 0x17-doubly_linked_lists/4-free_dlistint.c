06a9903013746adef27bb401797b5756fc28b855
Safuan04 <elmai.safuan@gmail.com>
2023-06-16 18:50:21 -0700

    free d-linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Func that frees a doubly linked list
+ * @head: Parameter
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
