cf5c0193a6dc4579f8c30e754d5e040891cbce5a
Safuan04 <elmail.safuan@gmail.com>
2023-10-15 02:12:13 +0100

    hot fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 /**
  * printing - this is a function that prints the array
- * @array:  is a pointer to the first element of the array to search in
+ * @array: is a pointer to the first element of the array to search in
  * @backward: search in mid left
  * @forward: search in mid right
 */
@@ -42,7 +42,7 @@ int binary_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-		while (backward <= forward)
+	while (backward <= forward)
 	{
 		printing(array, backward, forward);
 
